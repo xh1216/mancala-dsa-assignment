a93bb29f6a44c6d786129afe94bcb6c5c2a8e3d0
poopfart95 <choooo1095@gmail.com>
2019-05-28 23:16:23 +0800

    added gamepage.fxml, resultspage.fxml, holes.css

---
+++ src/Model/GamePage.java
@@ -0,0 +1,11 @@
+package Model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GamePage {
+    
+}
